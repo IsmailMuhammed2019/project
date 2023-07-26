1079c9feb78122d4c8f895d388e780cd3a24b4e7
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-07-26 11:50:17 +0100

    update setup file

--- setupfiles/setup.sh
+++ setupfiles/setup.sh
@@ -5,7 +5,8 @@ echo '192.168.56.11	control' >> /etc/hosts
 echo '192.168.56.12	node01' >> /etc/hosts
 echo '192.168.56.13	node02' >> /etc/hosts
 echo '192.168.56.14	node03' >> /etc/hosts
-
+echo '192.168.56.15	node04' >> /etc/hosts
+echo '192.168.56.16	node05' >> /etc/hosts
 
 
 
